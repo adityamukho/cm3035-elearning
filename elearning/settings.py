47f276437b399eb1b3aad633583ee8ba36778d91
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-06 08:48:11 +0530

    Added apps and first model skeleton.

--- elearning/settings.py
+++ elearning/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api.apps.ApiConfig",
+    "web.apps.WebConfig"
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
