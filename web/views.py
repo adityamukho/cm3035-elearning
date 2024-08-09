5ca92ab2cd2b52ceb1cdf037b80a789ce012a30e
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-09 06:00:07 +0530

    Homepage.

--- web/views.py
+++ web/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the web index.")
+def heartbeat(request):
+    return HttpResponse("alive")
+
+
+def home(request):
+    return render(request,'home.html')
