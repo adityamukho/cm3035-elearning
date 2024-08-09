78a107c4ab899be949e9dbfd02311a07b2773060
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-09 05:59:33 +0530

    Initial models.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Profile, Student, Teacher
+
+admin.site.register(Course)
+admin.site.register(Profile)
+admin.site.register(Student)
+admin.site.register(Teacher)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Student(Profile):
+    pass
+
+
+class Teacher(Profile):
+    pass
