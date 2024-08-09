b5f1fb154b1a9ffcd9e610d55158515987a23401
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-09 12:54:57 +0530

    Code cleanup.

--- uniworld/urls.py
+++ uniworld/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("heartbeat/", heartbeat, name="heartbeat"),
+    path('heartbeat/', heartbeat, name="heartbeat"),
     path("", home, name="home"),
     path('courses/', CourseListView.as_view(), name='courses'),
     path('course/<int:pk>/', CourseDetailView.as_view(), name='course'),
