9a129308cb6d9485726eb3854560de5ff16fb3c0
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-09 06:08:00 +0530

    Fixed heartbeat URL.

--- elearning/urls.py
+++ elearning/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("api/", include("api.urls")),
     path("web/", include("web.urls")),
     path('admin/', admin.site.urls),
 ]
