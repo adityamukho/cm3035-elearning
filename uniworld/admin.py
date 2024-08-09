fb688996ca6c3323fabfd1ad47327d91daaee8b4
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-09 12:55:15 +0530

    Removed profile models for now.

--- uniworld/admin.py
+++ uniworld/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 
-from uniworld.models import Course, Profile, Student, Teacher
+from uniworld.models import Course
 
 admin.site.register(Course)
-admin.site.register(Profile)
-admin.site.register(Student)
-admin.site.register(Teacher)
