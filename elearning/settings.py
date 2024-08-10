03113e5714571a59efa354b557e13c15d31f8e3b
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-10 09:31:48 +0530

    Media file support.

--- elearning/settings.py
+++ elearning/settings.py
@@ -126,3 +126,6 @@ LOGIN_REDIRECT_URL = 'courses'
 LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
