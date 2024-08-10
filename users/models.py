5b1682048aef4bb0647c511b080247ef9160cbf0
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-10 13:21:57 +0530

    Added profile model.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from PIL import Image
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(
+        default='avatar.jpg',  # default avatar
+        upload_to='profile_avatars'  # dir to store the image
+    )
+
+    def __str__(self):
+        # noinspection PyUnresolvedReferences
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        # save the profile first
+        super().save(*args, **kwargs)
+
+        # resize the image
+        img = Image.open(self.avatar.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            # create a thumbnail
+            img.thumbnail(output_size)
+            # overwrite the larger image
+            img.save(self.avatar.path)
