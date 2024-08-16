b3e85c4ee32dbb391c3cf3c2ad8e48aa1630596c
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-16 06:27:00 +0530

    Allow direct import of User.

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,8 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import Profile
-
-User = get_user_model()
 
 
 class RegisterForm(UserCreationForm):
--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from PIL import Image
-
-User = get_user_model()
 
 
 class Profile(models.Model):
