e4cd9d69cf68e1bad9ad9f1f0e81b6d47bb63ea5
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-08-27 14:13:27 +0530

    Inclued first name and last name fields.

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', ]
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', ]
 
 
 class UserUpdateForm(forms.ModelForm):
