6b1e33764179c97de2efdc9580d5b8179481b104
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-09-01 08:35:03 +0530

    403 template

---
+++ elearning/middleware.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+class Custom403Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code == 403:
+            return render(request, '403.html', status=403)
+        return response
--- elearning/settings.py
+++ elearning/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'elearning.middleware.Custom403Middleware',
 ]
 
 ROOT_URLCONF = 'elearning.urls'
