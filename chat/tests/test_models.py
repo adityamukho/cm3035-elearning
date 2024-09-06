c5cf0ea67e6722e6a7149772d2a26de70800ba18
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-09-06 21:11:51 +0530

    Permissions tests in chat models.

--- chat/tests/test_models.py
+++ chat/tests/test_models.py
@@ -7,6 +7,7 @@ User = get_user_model()
 class RoomModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
+        self.other_user = User.objects.create_user(username='otheruser', password='12345')
         self.room = Room.objects.create(name='Test Room', slug='test-room', creator=self.user)
 
     def test_room_creation(self):
@@ -17,9 +18,20 @@ class RoomModelTest(TestCase):
     def test_room_str_representation(self):
         self.assertEqual(str(self.room), 'Test Room')
 
+    def test_room_permissions(self):
+        # Test view permission
+        self.assertTrue(self.user.has_perm(Room.get_perm('view'), self.room))
+        self.assertTrue(self.other_user.has_perm(Room.get_perm('view'), self.room))
+
+        # Test add, change, and delete permissions (should always be denied)
+        self.assertFalse(self.user.has_perm(Room.get_perm('add'), Room))
+        self.assertFalse(self.user.has_perm(Room.get_perm('change'), self.room))
+        self.assertFalse(self.user.has_perm(Room.get_perm('delete'), self.room))
+
 class MessageModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
+        self.other_user = User.objects.create_user(username='otheruser', password='12345')
         self.room = Room.objects.create(name='Test Room', slug='test-room', creator=self.user)
         self.message = Message.objects.create(room=self.room, user=self.user, content='Test message')
 
@@ -32,3 +44,16 @@ class MessageModelTest(TestCase):
 
     def test_message_ordering(self):
         self.assertEqual(Message._meta.ordering, ('date_added',))
+
+    def test_message_permissions(self):
+        # Test view permission
+        self.assertTrue(self.user.has_perm(Message.get_perm('view'), self.message))
+        self.assertTrue(self.other_user.has_perm(Message.get_perm('view'), self.message))
+
+        # Test add permission
+        self.assertTrue(self.user.has_perm(Message.get_perm('add'), Message))
+        self.assertTrue(self.other_user.has_perm(Message.get_perm('add'), Message))
+
+        # Test change and delete permissions (should always be denied)
+        self.assertFalse(self.user.has_perm(Message.get_perm('change'), self.message))
+        self.assertFalse(self.user.has_perm(Message.get_perm('delete'), self.message))
