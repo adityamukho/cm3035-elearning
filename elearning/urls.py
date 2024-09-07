9bf2296ea93603e10d615767a0ac7913d791512c
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-09-07 15:21:24 +0530

    Trying to add static URLs

--- elearning/urls.py
+++ elearning/urls.py
@@ -24,8 +24,9 @@ urlpatterns = [
     path('chat/', include("chat.urls")),
     path('user/', include("users.urls")),
     path('admin/', admin.site.urls),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
