7187671a83574fe3ea37b344c9f7e1002a46cf7f
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-09-07 20:57:30 +0530

    Make executable

--- run-celery.sh
+++ run-celery.sh
@@ -2,11 +2,12 @@
 # Exit on error
 set -o errexit
 
-export DEBUG=True
+export DEBUG=False
 
-# export EMAIL_HOST=your_email_host
-# export EMAIL_HOST_PASSWORD=your_email_host_password
-# export EMAIL_HOST_USER=your_email_host_user
-# export EMAIL_PORT=your_email_port
+export EMAIL_HOST=your_email_host
+export EMAIL_HOST_PASSWORD=your_email_host_password
+export EMAIL_HOST_USER=your_email_host_user
+export EMAIL_PORT=your_email_port
 
-celery -A elearning.celery worker --loglevel=info
+
+celery -A elearning worker --loglevel=info
