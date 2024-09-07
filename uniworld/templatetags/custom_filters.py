51cad7ae29d9c15a454af97e09c5000b2a827b31
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-09-07 12:06:32 +0530

    Custom tag for star rating,

--- uniworld/templatetags/custom_filters.py
+++ uniworld/templatetags/custom_filters.py
@@ -1,7 +1,14 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def verbose_name(instance):
     return instance._meta.verbose_name
+
+@register.simple_tag
+def star_rating(rating, max_rating=5):
+    html = ''.join([f'<span class="star unfilled">☆</span>' for _ in range(max_rating - int(rating))] +
+                   [f'<span class="star filled">★</span>' for _ in range(int(rating))])
+    return mark_safe(html)
